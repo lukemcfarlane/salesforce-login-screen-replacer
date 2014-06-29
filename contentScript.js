536d91281bd88a8250fac4204b68f2a074da5b25
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-06-30 11:00:16 +1200

    How about we just replace the whole page? Who even likes it anyway?

--- contentScript.js
+++ contentScript.js
@@ -2,3 +2,19 @@ $('head').append($('<script src="https://code.jquery.com/jquery-git2.min.js"></s
 $('[src$="/jslibrary/LoginHint.js"]').remove()
 $('#chooser').remove();
 $('#theloginform').show();
+
+var newContentStr = '<body>' +
+	'        <form action="https://test.salesforce.com/" name="login" method="post">' +
+	'            <div>' +
+	'                <label for="un">Username:</label>' +
+	'                <input type="email" id="username" name="un"/>' +
+	'            </div>' +
+	'            <div>' +
+	'                <label for="pw">Password:</label>' +
+	'                <input type="password" id="password" name="pw"/>' +
+	'            </div>' +
+	'            <button type="submit">Log in</button>' +
+	'        </form>' +
+	'    </body>';
+
+$('html body').replaceWith(newContentStr);
