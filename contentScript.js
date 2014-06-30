2e369369c10bdea907b71da9c550762dc9d31cb8
James Hill <oblongmana@gmail.com>
2014-06-30 13:16:57 +1200

    Add some style

--- contentScript.js
+++ contentScript.js
@@ -2,20 +2,27 @@ var hostnameEndpoints = {
   'test.salesforce.com' : 'https://test.salesforce.com/',
   'login.salesforce.com' : 'https://login.salesforce.com/'
 };
+var hostnameTargetNames = {
+  'test.salesforce.com' : 'Sandbox',
+  'login.salesforce.com' : 'Production'
+};
 
 var endpoint = hostnameEndpoints[window.location.hostname];
+var targetName = hostnameTargetNames[window.location.hostname];
 
 var newContentStr = '<body style="display: block !important">' + //display reverses hide_at_start.css
-	'        <form action="' + endpoint + '" name="login" method="post">' +
+	'        <form id="mainForm" action="' + endpoint + '" name="login" method="post">' +
+  '            <img src="' + chrome.extension.getURL('trineo-logo.png') + '"/> ' +
+  '            <h2>Salesforce Login: ' + targetName + '</h2>' +
 	'            <div>' +
-	'                <label for="un">Username:</label>' +
-	'                <input type="email" id="username" name="un"/>' +
+	'                <label for="un" class="smooth">Username:</label>' +
+	'                <input type="email" id="username" name="un" class="smooth"/>' +
 	'            </div>' +
 	'            <div>' +
-	'                <label for="pw">Password:</label>' +
-	'                <input type="password" id="password" name="pw"/>' +
+	'                <label for="pw" class="smooth">Password:</label>' +
+	'                <input type="password" id="password" name="pw" class="smooth"/>' +
 	'            </div>' +
-	'            <button type="submit">Log in</button>' +
+	'            <button type="submit" class="smooth btn-small" style="background-color: DimGray; text-align: right;">Log in</button>' +
 	'        </form>' +
 	'    </body>';
 
